7d7c71daad13de4d864fa28e9fd96e7a6366e3b5
Asel <asel.alimbaeva.kk@gmail.com>
2022-03-10 09:00:25 +0300

    feat : this.status === 404

--- gh-pages/http/js.js
+++ gh-pages/http/js.js
@@ -3,11 +3,20 @@ document.getElementById("get-data").addEventListener("click", loadData);
 function loadData() {
     const xhr = new XMLHttpRequest();
 
-    xhr.open("GET", "db.txt", true);
+    xhr.open("GET", "dbe.txt", true);
 
     xhr.onload = function () {
         if (this.status === 200) {
             console.log(this.response);
+            document.querySelector(".output").innerHTML = `
+            <p>
+            ${this.response}
+            </p>
+            `
+        } else if (this.status === 404) {
+            alert("Can not connect to DB!!!")
+        } else {
+            alert("Server error!!!")
         }
     }
 
