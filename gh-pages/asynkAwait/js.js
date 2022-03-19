620cff093cf2b0f3bb1c17f5c8959506bcbe5c35
Asel <asel.alimbaeva.kk@gmail.com>
2022-03-19 20:06:02 +0300

    feat: added  data

--- gh-pages/asynkAwait/js.js
+++ gh-pages/asynkAwait/js.js
@@ -1,3 +1,6 @@
+
+const output = document.querySelector(".output");
+
 async function cart() {
     const res = await fetch("https://jsonplaceholder.typicode.com/user/1/posts");
 
@@ -7,8 +10,24 @@ async function cart() {
 }
 
 cart().then((posts) => {
-    console.log(posts)
-})
-    .catch((err) => {
-        console.error(err);
+    posts.forEach((post) => {
+        const cart = document.createElement("div");
+        cart.setAttribute("class", "cart");
+        const h2 = document.createElement("h2");
+        const p = document.createElement("p");
+
+        h2.innerHTML = `${post.title}`;
+        p.innerHTML = post.body;
+
+        cart.appendChild(h2);
+        cart.appendChild(p);
+
+        output.appendChild(cart);
+
+
     })
+
+}).catch((err) => {
+    console.error(err);
+})
+cart();
