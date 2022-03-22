41783b5f297e58c643ef45d02c8bc9d0acc67ada
Asel <asel.alimbaeva.kk@gmail.com>
2022-03-22 12:01:06 +0300

    feat: add class Github

--- gh-pages/PracticGitHubDataBase/git.js
+++ gh-pages/PracticGitHubDataBase/git.js
@@ -1,12 +1,12 @@
 
 class Github {
     constructor() {
-        this.client_id = "d4c2979dccaa9c2866a8";
-        this.client_secret = "ae3660c9925530b6b9fdb29d51dc244883a5c468";
+        // this.client_id = "d4c2979dccaa9c2866a8";
+        // this.client_secret = "ae3660c9925530b6b9fdb29d51dc244883a5c468";
     }
 
     async getUser(user) {
-        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${client_id}&client_secret=${client_secret}`);
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
 
         const profile = await profileResponse.json();
 
